3abb9ccb853bbe15887a6c512a87415473e2db58
Dmitry Kunin <dkunin@avito.ru>
2017-03-24 17:16:29 +0300

    master:Initial commit

---
+++ app/src/renderer/components/LandingPageView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <imageViewer />
+  </div>
+</template>
+
+<script>
+
+  import imageViewer from './image-viewer';
+
+  export default {
+    components: {
+      imageViewer,
+    },
+    name: 'landing-page',
+  };
+</script>
+
+<style scoped>
+</style>
---
+++ app/src/renderer/components/image-viewer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="big-image">
+      <img :src="images[currentImage]" alt="">
+    </div>
+    <div class="thumbs" v-for="(singleImage,index) in images">
+      <img :src="singleImage" @click.prevent="onSelectImage(index)"/>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+  .thumbs {
+    display: inline-block;
+  }
+  .thumbs img {
+    max-width: 80px;
+  }
+</style>
+
+<script>
+  export default {
+    name: 'image-viewer',
+    data() {
+      return {
+        currentImage: 0,
+        images: [
+          'http://lorempixel.com/400/200/sports/1', 'http://lorempixel.com/400/200/sports/2',
+        ],
+      };
+    },
+    methods: {
+      onSelectImage(index) {
+        this.currentImage = index;
+      },
+    },
+  };
+</script>
